816008452631cb47653164d986a21a75baa08b95
lashum <lashum@rpi.edu>
2024-12-04 13:52:41 -0500

    my part of paper

--- CausalAnalysis/Final/ConstructionPriceIndex/process.R
+++ CausalAnalysis/Final/ConstructionPriceIndex/process.R
@@ -2,6 +2,8 @@ setwd("~/Coursework/DatEcon/Project/Econometrics_Project/CausalAnalysis/Final/Co
 library(tidyverse)
 library(readxl)
 
+rename_lower = function(x) rename_with(x, str_to_lower, everything())
+
 sold_ind = read_xlsx("price_sold_cust.xlsx") %>% 
   select(Year, starts_with("Q")) %>% 
   pivot_longer(starts_with("Q"), names_to = "Quarter", 
@@ -10,7 +12,7 @@ sold_ind = read_xlsx("price_sold_cust.xlsx") %>%
          date_string = str_c(Year, " ", (Quarter-1)*3 + 1),
          date = ym(date_string)) %>% 
   select(-date_string) %>% 
-  rename_with(str_to_lower, everything()) %>% 
+  rename_lower() %>% 
   na.omit()
 
 constr_ind = read_xlsx("price_uc_cust.xlsx") %>% 
@@ -19,23 +21,29 @@ constr_ind = read_xlsx("price_uc_cust.xlsx") %>%
   select(date, constr_price_ind)
 
 mspus = read_csv("MSPUS.csv") %>% 
-  rename_with(str_to_lower, everything())
+  rename_lower()
 
 msptfc = read_csv("MSPUS_cash.csv") %>% 
-  rename_with(str_to_lower, everything())
+  rename_lower()
 
 fmr = read_csv("FMR/FMR.csv") %>% 
   group_by(year) %>% 
   summarize(fmr_1 = mean(fmr_1))
+
+cpi = read_csv("CPIU.csv") %>% 
+  rename_lower() %>% 
+  rename(cpi = cpiaucsl) %>% 
+  mutate(date = ymd(date))
 
 constr_data = sold_ind %>% 
   inner_join(mspus) %>%
   inner_join(msptfc) %>%
   inner_join(constr_ind) %>% 
   inner_join(fmr) %>% 
-  mutate(sold_price_ind = sold_price_ind,
-         constr_price_ind = constr_price_ind,
+  inner_join(cpi) %>% 
+  mutate(cpi_mspus = (mspus / cpi),
          sold_corrected = (mspus / sold_price_ind),
+         cpi_sold = (sold_price_ind / cpi),
          constr_corrected = (mspus / constr_price_ind),
          sold_corrected_cash = (msptfc / sold_price_ind),
          constr_corrected_cash = (msptfc / constr_price_ind),
